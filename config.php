bfb24f73d42629fa063359ab540d90d3e372d77a
jnatmed <juanmannat@gmail.com>
2022-11-11 00:26:30 -0300

    agregando config.php

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ use Monolog\Logger as MonologLogger;
 
 return [
     'database' => [
-        'name' => 'escuela',
-        'username' => 'root',
-        'password' => 'Y00s4d14',
-        'connection' => 'mysql:host=127.0.0.1',
+        'name' => 'id19108229_escuela',
+        'username' => 'id19108229_juan',#'root',
+        'password' => 'GZ$y7PM=_)!7$p)r',#'Y00s4d14',
+        'connection' => 'www.000webhost.com',#'mysql:host=127.0.0.1',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]
