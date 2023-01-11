c47c0cd1b1d1685b557cfb908c06ab58110afa6c
jnatmed <juanmannat@gmail.com>
2023-01-11 00:01:51 -0300

    modificando config.php template_dir

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ use Monolog\Logger as MonologLogger;
 return [
     'database' => [
         'name' => 'escuela',
-        'username' => 'admin',#'root',
-        'password' => 'admin',#'Y00s4d14',
+        'username' => 'root',#'root',
+        'password' => 'defuru31KO',#'Y00s4d14',
         'connection' => 'mysql:host=168.181.185.59',#'mysql:host=127.0.0.1',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
@@ -16,7 +16,7 @@ return [
         'level' => MonologLogger::INFO
     ],
     'twig' => [
-        'templates_dir' => __DIR__ . '/../app/views',
-        'templates_cache_dir' => __DIR__ . '/../app/views/cache'
+        'templates_dir' => __DIR__ . 'app/views',
+        'templates_cache_dir' => __DIR__ . 'app/views/cache'
     ]
 ];
