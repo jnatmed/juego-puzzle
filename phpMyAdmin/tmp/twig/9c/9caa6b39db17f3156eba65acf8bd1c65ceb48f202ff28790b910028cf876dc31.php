bcbb5159e5679c383f36f7e52cb2062da11fe910
jnatmed <49349742+jnatmed@users.noreply.github.com>
2023-01-15 01:36:17 -0300

    agregando phpMyAdmin

---
+++ phpMyAdmin/tmp/twig/9c/9caa6b39db17f3156eba65acf8bd1c65ceb48f202ff28790b910028cf876dc31.php
@@ -0,0 +1,77 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* secondary_tabs.twig */
+class __TwigTemplate_8cf097800540e50a53bbea21511b0b4ab4fb06064409a483ffb6142643c79839 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<ul id=\"topmenu2\">
+    ";
+        // line 2
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["sub_tabs"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["tab"]) {
+            // line 3
+            echo "        ";
+            echo PhpMyAdmin\Util::getHtmlTab($context["tab"], ($context["url_params"] ?? null));
+            echo "
+    ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['tab'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 5
+        echo "</ul>
+<div class=\"clearfloat\"></div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "secondary_tabs.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  53 => 5,  44 => 3,  40 => 2,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "secondary_tabs.twig", "C:\\Users\\FamiliaNatelloMedina\\Documents\\UNLu\\BasedeDatosMasivas\\SoftWare\\phpMyAdmin-5.0.3-all-languages\\templates\\secondary_tabs.twig");
+    }
+}
