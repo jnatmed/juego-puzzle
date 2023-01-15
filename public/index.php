60ca4f1e5b5f12b3c3b194eb204eb9a95fda7f21
jnatmed <juanmannat@gmail.com>
2023-01-15 13:04:33 -0300

    arreglando rutas - x haber movido index.php al public

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-require __DIR__ . '/../core/bootstrap.php';
+require_once __DIR__ . '/../core/bootstrap.php';
 
 
 use App\Core\App;
