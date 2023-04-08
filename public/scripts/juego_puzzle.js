f3341c5306cc826d37586e622b9da499b7ac9b0e
jnatmed <juanmannat@gmail.com>
2023-04-08 00:50:49 -0300

    cambios en front
    la imagen se obtiene de forma random, pero no puedo colocarla en el canvas

--- public/scripts/juego_puzzle.js
+++ public/scripts/juego_puzzle.js
@@ -1,20 +1,23 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var image = document.getElementById('source');
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+let image = document.getElementById('imagen');
 
-ctx.drawImage(image, 150, //sx: 
-  100, //sy: 
-  20, //sWidth
-  50, //sHeight
-  25,  //dx: coordenada X del canvas destino en la cual se coloca la esquina superior
-  20, //dy: coordenada Y del canvas destino en la cual se coloca la esquina superior izquierda
-  100, //dWidth
-  604 //dHeight
+console.log(image.hidden);
+
+let aux = 10;
+
+ctx.drawImage(image, 
+              0, 0, // hoja de mosaico x, y (esquina superior izquierda de la captura) 
+              aux, aux, //Que tanto va a crecer la imagen
+              0, 0, //Donde quieres que se coloque la imagen
+              aux, aux //dHeight
 );
 
+console.log("Image: ANCHO: " + image.width + "\n LARGO: " + image.height);
 console.log("Canvas: ANCHO: " + canvas.width + "\n LARGO: " + canvas.height);
 
 canvas.addEventListener('click', function (event) {
-  alert("coord X: " + event.pageX + " - coord Y: " + event.pageY);
+  // alert("coord X: " + event.offsetX + " - coord Y: " + event.offsetY);
+  console.log("coord X: " + event.offsetX + " - coord Y: " + event.offsetY);
 });
 
