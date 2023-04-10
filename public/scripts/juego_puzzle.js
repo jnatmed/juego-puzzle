e4c2cd6d425f651dd9e28ff70641384e01cc2cc8
jnatmed <49349742+jnatmed@users.noreply.github.com>
2023-04-10 09:51:31 -0300

    probando appendChild
    logre mover un canvas añadiendolo a la rama otra fila de una lista.

--- public/scripts/juego_puzzle.js
+++ public/scripts/juego_puzzle.js
@@ -1,41 +1,53 @@
 
 class Juego {
 
-  dibujarImagenEnCanvas(idCanvas, xOrigen, yOrigen){
+  dibujarImagenEnCanvas(idCanvas, xOrigen, yOrigen) {
     let canvas = document.getElementById(idCanvas);
-    let ctx = canvas.getContext("2d");
+    let context = canvas.getContext("2d");
     let image = document.getElementById("imagen");
-    
-    let [anchoOrigen, anchoDestino, altoOrigen, altoDestino] = [100,100,100,100];              
-    let [xDestino, yDestino] = [0,0];
-  
-    ctx.drawImage(image,
-                xOrigen, yOrigen, //coordenada x e y en imagen origen
-                anchoOrigen, altoOrigen, //cant pixeles en ancho y alto que quiero tomar
-                xDestino, yDestino, //coordenada x e y en destino
-                anchoDestino, altoDestino//cuanto va a ocupar la imagen en destino
+
+    let [anchoOrigen, anchoDestino, altoOrigen, altoDestino] = [100, 100, 100, 100];
+    let [xDestino, yDestino] = [0, 0];
+
+    context.drawImage(image,
+      xOrigen, yOrigen, //coordenada x e y en imagen origen
+      anchoOrigen, altoOrigen, //cant pixeles en ancho y alto que quiero tomar
+      xDestino, yDestino, //coordenada x e y en destino
+      anchoDestino, altoDestino//cuanto va a ocupar la imagen en destino
     );
   }
 }
 
+function $(idDiv) {
+  return document.getElementById(idDiv);
+}
+
 const juego = new Juego();
 
-const coordXOrigen = [0,101,201];
-const coordYOrigen = [0,101,201];
+const coordXOrigen = [0, 101, 201];
+const coordYOrigen = [0, 101, 201];
 
 let fila = 0;
 
-coordYOrigen.forEach(function(coordY){
-    // console.log('[foreach] y:', coordY);
-    coordXOrigen.forEach(function(coordX, i){
+coordYOrigen.forEach(function (coordY) {
+  // console.log('[foreach] y:', coordY);
+  coordXOrigen.forEach(function (coordX, i) {
 
-      // console.log('canvas_'+ (i + fila) +' (' +  coordX + "," + coordY + ')');
-      juego.dibujarImagenEnCanvas("canvas_" + (i + fila), coordX, coordY);
-    });
-    fila = fila + 3;
+    // console.log('canvas_'+ (i + fila) +' (' +  coordX + "," + coordY + ')');
+    juego.dibujarImagenEnCanvas("canvas_" + (i + fila), coordX, coordY);
   });
-  
-  
+  fila = fila + 3;
+});
+
+let pieza_0 = $("canvas_0");
+let pieza_1 = $("canvas_1");
+let pieza_2 = $("canvas_2");
+let div_8 = $("2");
+
+div_8.appendChild(pieza_0);
+div_8.appendChild(pieza_1);
+div_8.appendChild(pieza_2);
+
 // juego.dibujarImagenEnCanvas("canvas_0",0,0);
 // juego.dibujarImagenEnCanvas("canvas_1",101,0);
 // juego.dibujarImagenEnCanvas("canvas_2",201,0);
@@ -52,16 +64,16 @@ coordYOrigen.forEach(function(coordY){
 // console.log("Image: ANCHO: " + imagen.width + "\n LARGO: " + imagen.height);
 // console.log("Canvas: ANCHO: " + canvas.width + "\n LARGO: " + canvas.height);
 
-// canvas.addEventListener('click', function (event) { 
-//   // alert("coord X: " + event.offsetX + " - coord Y: " + event.offsetY);
+// canvas.addEventListener('click', function (event) {
+  // alert("coord X: " + event.offsetX + " - coord Y: " + event.offsetY);
 //   console.log("coord X: " + event.offsetX + " - coord Y: " + event.offsetY);
 // });
-// canvas.addEventListener('ontouch', function (event) { 
-//   // alert("coord X: " + event.offsetX + " - coord Y: " + event.offsetY);
+// canvas.addEventListener('ontouch', function (event) {
+   // alert("coord X: " + event.offsetX + " - coord Y: " + event.offsetY);
 //   console.log("coord X: " + event.offsetX + " - coord Y: " + event.offsetY);
 // });
-// canvas.addEventListener('down', function (event) { 
-//   // alert("coord X: " + event.offsetX + " - coord Y: " + event.offsetY);
+// canvas.addEventListener('down', function (event) {
+  // alert("coord X: " + event.offsetX + " - coord Y: " + event.offsetY);
 //   console.log("coord X: " + event.offsetX + " - coord Y: " + event.offsetY);
 // });
 
